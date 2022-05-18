2acdb655a3cc5e18ab71f5da06a433d06b19dee0
Nina Brissey <nbrissey@gmail.com>
2022-05-18 09:18:33 -0700

    Removed token and changed process.env to .env

--- src/components/QAForm/QAForm.js
+++ src/components/QAForm/QAForm.js
@@ -26,8 +26,7 @@ const QAForm = ({ setQA, QA }) => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          // Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
         },
         body: JSON.stringify(data),
       }
