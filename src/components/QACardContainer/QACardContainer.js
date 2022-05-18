870f1164f23e9e9b10ee1f9f0bb0a89bcff910e2
Nina Brissey <nbrissey@gmail.com>
2022-05-17 22:13:36 -0700

    Setup QAContainer and pass props down

--- src/components/QACardContainer/QACardContainer.js
+++ src/components/QACardContainer/QACardContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const QACardContainer = () => {
+const QACardContainer = ({ QA }) => {
   return <div>QACardContainer</div>;
 };
 
--- src/components/QAContainer/QAContainer.js
+++ src/components/QAContainer/QAContainer.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QACardContainer from '../QACardContainer/QACardContainer';
+import QAForm from '../QAForm/QAForm';
 
 const QAContainer = () => {
-  return <div>QAContainer</div>;
+  const [QA, setQA] = useState([]);
+
+  return (
+    <section>
+      <QAForm setQA={setQA} QA={QA} />
+      <QACardContainer QA={QA} />
+    </section>
+  );
 };
 
 export default QAContainer;
