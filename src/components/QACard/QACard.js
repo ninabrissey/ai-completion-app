9d7085eaef908de0e4dc6b1bf2dfb8f2576a736f
Nina Brissey <nbrissey@gmail.com>
2022-05-17 23:41:50 -0700

    Add line break css rule

--- src/components/QACard/QACard.js
+++ src/components/QACard/QACard.js
@@ -5,7 +5,11 @@ const QACard = ({ qa }) => {
   useEffect(() => {
     console.log(qa);
   }, [qa]);
-  return <div className="qa-card">{qa && <p>{qa.text}</p>}</div>;
+  return (
+    <div className="qa-card">
+      {qa && <p className="display-linebreak">{qa}</p>}
+    </div>
+  );
 };
 
 export default QACard;
