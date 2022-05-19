7b2f05005f59f8b8bd341a7554d9ad752ae0bf7d
Nina Brissey <nbrissey@gmail.com>
2022-05-18 17:15:16 -0700

    Add application message to inform use case

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import QAContainer from '../QAContainer/QAContainer';
 
@@ -7,7 +8,14 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <p>Are you frequently asking yourself questions...?</p>
+      <p>Well, get those questions out of your mind and onto the screen.</p>
+      <p>
+        The openAI assistant will hopefully be able to help you out in a small
+        or big way...
+      </p>
       <QAContainer />
+      <Footer />
     </div>
   );
 }
